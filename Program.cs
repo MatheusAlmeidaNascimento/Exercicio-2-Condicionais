052e6e9714656310ae18b1ab7ef75cbfeb039e03
MatheusAlmeidaNascimento <86276081+MatheusAlmeidaNascimento@users.noreply.github.com>
2021-06-25 21:21:10 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercicio_2_condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           int anoAtual;
+           int mesAtual;
+           int idadeDia;
+           int idadePessoa;
+           float idadeSemana;
+           int anoNasceu;
+           int mesNasceu;
+
+
+           Console.WriteLine("Em qual ano você nasceu?");
+           anoNasceu = int.Parse(Console.ReadLine());
+           
+           Console.WriteLine("Em qual mês você nasceu?");
+           mesNasceu = int.Parse(Console.ReadLine());
+
+           anoAtual = DateTime.Now.Year;
+           mesAtual = DateTime.Now.Month;
+
+           idadePessoa = anoAtual - anoNasceu;
+           idadeDia = idadePessoa * 365;
+           idadeSemana = idadeDia / 7;
+           
+          
+               Console.WriteLine("A sua idade é de: " + idadePessoa);
+               Console.WriteLine("A sua idade em semanas é de: " + idadeSemana);
+           
+        }
+    }
+}
